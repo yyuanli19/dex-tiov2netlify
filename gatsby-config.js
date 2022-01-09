208b44f2d1d9a8e1403cff624a86d195fc6d1640
yyuanli19 <yyuanli19@gmail.com>
2022-01-09 19:53:32 +0100

    added blog section

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,21 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.dex-t.io",
     title: "dex-tnetlify",
+    description:
+        'dex-t.io, helps you integrate the right data science technologies into your business, using the right data',
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-styled-components"],
+  plugins: ["gatsby-plugin-netlify-cms",
+            "gatsby-plugin-styled-components",
+            "gatsby-plugin-image",
+            "gatsby-plugin-sharp",
+            {
+              resolve: "gatsby-source-filesystem",
+              options: {
+                name: `blog`,
+                path: `${__dirname}/blog`,
+              }
+            },
+            "gatsby-plugin-mdx",
+    ],
 };
